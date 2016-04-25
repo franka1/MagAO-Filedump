cee5d14ac3afb086d216b3d9f53120524b3b0a6b
franka1 <franka1@email.arizona.edu>
2016-04-25 13:40:05 -0700

    Added params for corner size.

--- Mag_center.py
+++ Mag_center.py
@@ -6,91 +6,95 @@ from astropy.io import fits
 import numpy as np
 import sys
 
-inpath = str(sys.argv[1])
-file_name = str(sys.argv[2])
-outpath = str(sys.argv[3])
-fwhm = float(sys.argv[4])
-r = float(sys.argv[5])
-# threshold = float(sys.argv[5])
-# r = float(sys.argv[6])
+def calc_threshold(corner_width,corner_height):
+    sum = 0.0
+    n = 0
+    #first corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += image[i][j]
+            n += 1
+    #second corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += image[len(image)-i-1][j]
+            n += 1
+    #thrid corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += image[i][len(image)-j-1]
+            n += 1
+    #fourth corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += image[len(image)-i-1][len(image)-j-1]
+            n += 1
 
-hdulist = fits.open(inpath + file_name)
-image = hdulist[0].data
+    mu = sum/n
+    sum = 0
 
-#calc threshold
-sum = 0.0
-n = 0
-#first corner
-for i in range(99):
-    for j in range(99):
-        sum += image[i][j]
-        n += 1
-#second corner
-for i in range(99):
-    for j in range(99):
-        sum += image[len(image)-i-1][j]
-        n += 1
-#thrid corner
-for i in range(99):
-    for j in range(99):
-        sum += image[i][len(image)-j-1]
-        n += 1
-#fourth corner
-for i in range(99):
-    for j in range(99):
-        sum += image[len(image)-i-1][len(image)-j-1]
-        n += 1
+    #first corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += (image[i][j] - mu)**2
+    #second corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += (image[len(image)-i-1][j] - mu)**2
+    #thrid corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += (image[i][len(image)-j-1] - mu)**2
+    #fourth corner
+    for i in range(corner_width):
+        for j in range(corner_height):
+            sum += (image[len(image)-i-1][len(image)-j-1] - mu)**2
 
-mu = sum/n
-sum = 0
+    sigma2 = sum/(n-1)
+    sigma = sigma2**(0.5)
+    threshold = 5*sigma
+    return threshold
 
-#first corner
-for i in range(99):
-    for j in range(99):
-        sum += (image[i][j] - mu)**2
-#second corner
-for i in range(99):
-    for j in range(99):
-        sum += (image[len(image)-i-1][j] - mu)**2
-#thrid corner
-for i in range(99):
-    for j in range(99):
-        sum += (image[i][len(image)-j-1] - mu)**2
-#fourth corner
-for i in range(99):
-    for j in range(99):
-        sum += (image[len(image)-i-1][len(image)-j-1] - mu)**2
+def main():
+    inpath = str(sys.argv[1])
+    file_name = str(sys.argv[2])
+    outpath = str(sys.argv[3])
+    fwhm = float(sys.argv[4])
+    r = float(sys.argv[5])
+    corner_width = int(sys.argv[6])
+    corner_height = int(sys.argv[7])
 
-sigma2 = sum/(n-1)
-sigma = sigma2**(0.5)
-threshold = 5*sigma
-print threshold
+    hdulist = fits.open(inpath + file_name)
+    image = hdulist[0].data
 
-from photutils import daofind
-from astropy.stats import mad_std
-bkg_sigma = mad_std(image)
-#print bkg_sigma
-#bkg_sigma = 2
-sources = daofind(image, threshold*bkg_sigma, fwhm)
+    threshold = calc_threshold(corner_width,corner_height)
 
-sources_2 = np.array(sources["id", "xcentroid", "ycentroid", "sharpness", "roundness1", "roundness2", "npix", "sky", "peak", "flux", "mag"])
-print_line= (file_name+","+str(sources_2))
-file= open(outpath, "a")
-file.write(print_line)
-file.close()
+    from photutils import daofind
+    from astropy.stats import mad_std
+    bkg_sigma = mad_std(image)
+    sources = daofind(image, threshold*bkg_sigma, fwhm)
 
-from photutils import aperture_photometry, CircularAperture
-positions = (sources['xcentroid'], sources['ycentroid'])
-apertures = CircularAperture(positions, r)
-phot_table = aperture_photometry(image, apertures)
-phot_table_2 = np.array(phot_table["aperture_sum", "xcenter", "ycenter"])
-print_line= (","+str(phot_table_2)+"\n")
-file= open(outpath, "a")
-file.write(print_line)
-file.close()
+    sources_2 = np.array(sources["id", "xcentroid", "ycentroid", "sharpness", "roundness1", "roundness2", "npix", "sky", "peak", "flux", "mag"])
+    print_line= (file_name+","+str(sources_2))
+    file= open(outpath, "a")
+    file.write(print_line)
+    file.close()
 
-import matplotlib.pylab as plt
-image[image<=0]=0.0001
-plt.imshow(image, cmap='gray', origin='lower')
-apertures.plot(color='blue', lw=1.5, alpha=0.5)
-plt.show()
+    from photutils import aperture_photometry, CircularAperture
+    positions = (sources['xcentroid'], sources['ycentroid'])
+    print positions
+    apertures = CircularAperture(positions, r)
+    phot_table = aperture_photometry(image, apertures)
+    phot_table_2 = np.array(phot_table["aperture_sum", "xcenter", "ycenter"])
+    print_line= (","+str(phot_table_2)+"\n")
+    file= open(outpath, "a")
+    file.write(print_line)
+    file.close()
+
+    import matplotlib.pylab as plt
+    image[image<=0]=0.0001
+    plt.imshow(image, cmap='gray', origin='lower')
+    apertures.plot(color='blue', lw=1.5, alpha=0.5)
+    plt.show()
+
+main
