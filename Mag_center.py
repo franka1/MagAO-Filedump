e4b240681bdfb0c0db870a55a2731c6d92c6da84
franka1 <franka1@email.arizona.edu>
2016-04-27 10:45:34 -0700

    Multifile processing added

--- Mag_center.py
+++ Mag_center.py
@@ -6,6 +6,10 @@ from astropy.io import fits
 import numpy as np
 import sys
 import os
+from photutils import daofind
+from astropy.stats import mad_std
+from photutils import aperture_photometry, CircularAperture
+import matplotlib.pylab as plt
 
 def calc_threshold(image, corner_width, corner_height):
     sum = 0.0
@@ -56,25 +60,13 @@ def calc_threshold(image, corner_width, corner_height):
     threshold = 5*sigma
     return threshold
 
-def main():
-    inpath = str(sys.argv[1])
-    file_name = str(sys.argv[2])
-    outpath = str(sys.argv[3])
-    fwhm = float(sys.argv[4])
-    r = float(sys.argv[5])
-    corner_width = int(sys.argv[6])
-    corner_height = int(sys.argv[7])
 
-    file_names = os.listdir(inpath)
-    for file_name in file_names:
-
+def process_file(inpath, file_name, corner_width, corner_height, fwhm, r, outpath):
     hdulist = fits.open(inpath + file_name)
     image = hdulist[0].data
 
     threshold = calc_threshold(image, corner_width, corner_height)
 
-    from photutils import daofind
-    from astropy.stats import mad_std
     bkg_sigma = mad_std(image)
     sources = daofind(image, threshold*bkg_sigma, fwhm)
 
@@ -85,7 +77,6 @@ def main():
     file.write(print_line)
     file.close()
 
-    from photutils import aperture_photometry, CircularAperture
     positions = (sources['xcentroid'], sources['ycentroid'])
     print positions
     apertures = CircularAperture(positions, r)
@@ -96,11 +87,28 @@ def main():
     file.write(print_line)
     file.close()
 
-    import matplotlib.pylab as plt
     image[image<=0]=0.0001
     plt.imshow(image, cmap='gray', origin='lower')
     apertures.plot(color='blue', lw=1.5, alpha=0.5)
     plt.show()
+
+
+def main():
+    inpath = str(sys.argv[1])
+#    file_name = str(sys.argv[2])
+    outpath = str(sys.argv[2])
+    fwhm = float(sys.argv[3])
+    r = float(sys.argv[4])
+    corner_width = int(sys.argv[5])
+    corner_height = int(sys.argv[6])
+
+    if os.path.isdir(inpath):
+        file_names = os.listdir(inpath)
+        for file_name in file_names:
+            process_file(inpath, file_name, corner_width, corner_height, fwhm, r, outpath)
+
+    else:
+        process_file(inpath, "", corner_width, corner_height, fwhm, r, outpath)
 
 if __name__ == "__main__":
 	main()
